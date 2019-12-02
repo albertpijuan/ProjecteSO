90597ac2e54c2678bc6f1fbdcfd1924fc8ee79fd
Junki22 <ory__22@hotmail.com>
2019-12-02 19:59:51 +0100

    Classe Simulació

--- S0_projecte/src/Simulacio.java
+++ S0_projecte/src/Simulacio.java
@@ -1,4 +1,44 @@
 public class Simulacio
 {
-    
+    private Proces[] arrayProces;
+    private int casellaActual;
+    private int quantum;
+
+    //Mètode de llegir fitxer
+    public String llegirFitxer()
+    {
+        return "a";
+    }
+
+    //Canviar l'estat del procès
+    public void changeState(int state, Proces proces)
+    {
+
+    }
+
+    //Parar la simulació
+    public void stop()
+    {
+
+    }
+
+    //Continuar la simulació
+    public void continue_()
+    {
+
+    }
+
+    //Resetejar la simulació
+    public void restart()
+    {
+
+    }
+
+
+    //Triar entre diferents planificadors
+    public void chosePlan()
+    {
+
+    }
+
 }
