a7dbabdd71d13dab707cbb8b703c52bdc316c6f0
Albert Pijuan <apijuan12@hotmail.com>
2019-12-16 18:04:22 +0100

    Albert i David hem afegit una pantalla d'entrada de dades (Round Robin).

---
+++ Diseny/src/EntradaDadesRoundRobin.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EntradaDadesRoundRobin {
+    private JPanel contentPane;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton buttonMes;
+    private JButton okButton;
+    private JButton clearButton;
+
+    public EntradaDadesRoundRobin() {
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonMes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
